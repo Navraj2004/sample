1748b2b1cce2994a624d3a98f03dcde426a797f3
Navraj Giri <144989391+Navraj2004@users.noreply.github.com>
2025-04-02 14:43:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+def find_s(training_data):
+    
+    h = training_data[0][:-1]
+    
+    for example in training_data:
+        if example[-1] == "yes":
+            for i in range(len(h)):
+                if(h[i]!=example[i]):
+                    h[i] = "?"
+                
+    return h
+
+training_data = [
+    ['sunny','warm','normal','strong','yes'],
+    ['sunny','warm','high','strong','yes'],
+    ['rainy','cold','high','strong','no'],
+    ['sunny','warm','high','strong','yes']
+]
+
+h = find_s(training_data)
+print(h)
+ 
